e46e3f69cfaa6b42ad465b1f0c5c32f031ee801e
Shantanu Gupta <shantanufrom4387@gmail.com>
2020-09-06 13:00:04 +0530

    completed card creation functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
@@ -7,21 +7,23 @@ class App extends Component {
     super();
     this.state = {
       monsters: [
-        { id: 1, name: 'Frankenstein' }, 
-        { id: 2, name: 'Dracula' }, 
+        { id: 1, name: 'Frankenstein' },
+        { id: 2, name: 'Dracula' },
         { id: 3, name: 'Zombie' }]
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+      .catch(error => console.log(error));
   }
 
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(m => (
-              <h1 key={m.id}>{m.name}</h1>
-            ))
-        }
-        <button onClick={() => this.setState({ newProp: 'Just changed' })}>Change Me!</button>
+        <CardList monsters={this.state.monsters}/>
       </div>
     );
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from '../card/card.component';
+
+import './card-list.styles.css';
+
+export const CardList = props => (
+        <div className="card-list">
+            {
+                props.monsters.map(m => (
+                    <Card key={m.id} monster={m}></Card>
+                ))
+            }
+        </div>
+)
