56b4f6457410d1941b1a6f02fd75140e6de3edb6
Shantanu Gupta <shantanu.gupta@live.com>
2020-11-03 06:31:24 +0530

    Updates

---
+++ precache-manifest.4009d3759fc73c02753f899bb4a7c6d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d548a8e841d882383268b95d1c7b0e2c",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e759ec29b7029c193bea",
+    "url": "/monster-rolodex/static/css/main.43059d5a.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e759ec29b7029c193bea",
+    "url": "/monster-rolodex/static/js/main.9cffa94c.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  }
+]);
